f2e6f7e0415e691e46dd112251a8a2a54c9ffc8b
saranasr83 <saranasr83@gmail.comgit config --global user.email saranasr83@gmail.comgit config --global user.email saranasr83@gmail.com>
2018-05-30 16:53:33 -0700

    created cli table

--- bamazon.sql
+++ bamazon.sql
@@ -4,11 +4,11 @@ CREATE database bamazonDB;
 USE bamazonDB;
 
 CREATE TABLE products (
-  item_id INT(100) NOT NULL auto_increment,
+  Item_id INT(100) NOT NULL auto_increment,
   Product_name VARCHAR(100) NOT NULL,
-  department_name VARCHAR(100) NOT NULL,
-  price FLOAT(10,2) NOT NULL,
-  stock_quantity FLOAT(10,2) NOT NULL,
+  Department_name VARCHAR(100) NOT NULL,
+  Price FLOAT(10,2) NOT NULL,
+  Stock_quantity FLOAT(10,2) NOT NULL,
   PRIMARY KEY (item_id)
 );
 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require('cli-table');
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -12,20 +13,51 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    userMenu()
+    print()
 })
 
-function userMenu() {
+function print() {
+    connection.query("select * from products", function (err, res) {
+
+        // instantiate
+        var table = new Table({
+            head: ['ID', 'Product', 'Department', 'Price', 'In Stock']
+            , colWidths: [10, 12, 15, 10, 12]
+        });
+
+        // table is an Array, so you can `push`, `unshift`, `splice` and friends
+        for (var i = 0; i < res.length; i++) {
+            table.push(
+                [res[i].Item_id, res[i].Product_name, res[i].Department_name, res[i].Price, res[i].Stock_quantity]
+
+            );
+        }     
+
+        console.log(table.toString());
+        //console.log(res)
+        userMenu()
+    })
+}
+
+function userMenu(res) {
     inquirer.prompt([
         {
-            type:"input",
-            name:"productId",
-            message:"Please enter the product ID that you would like to buy"
-       }, {
-            type:"input",
+            type: "input",
+            name: "productId",
+            message: "Please enter the product ID that you would like to buy",
+
+            validate: function (value) {
+                if (isNaN(value) === false && value <= res.length && value > 0) {
+                    return true;
+                }
+                console.log("The ID that you are looking for is not available. pLease Try Again!");
+            }
+        }, {
+            type: "input",
             name: "productquantity",
             message: "How many of that product are you willing to buy?"
-        }]).then(function(answer){
+        }]).then(function (answer) {
             console.log(answer);
+            
         })
 }
